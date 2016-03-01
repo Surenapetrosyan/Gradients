db2545c3cc71e2497ba9997943f3ed2931ed66d0
Surenapetrosyan <surenapetrosyan@gmail.com>
2016-03-01 11:36:01 -0800

    Twitter logo change

--- js/js.js
+++ js/js.js
@@ -146,7 +146,7 @@ bootstrap_alert = function() {}
 
 //Creates alert with extra class I created for css export styling.
 bootstrap_alert.warningWithCode = function(message) {
-            $('#alert_placeholder').html('<div class="alert alert-success alert-dismissable code"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><span>'+message+'</span></div>');
+            $('#alert_placeholder').html('<div class="alert alert-success alert-dismissable code"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><span>'+message+'</span> <hr> <p class="share">Like what I do here? I do it for the <a href="http://twitter.com/share?hashtags=WebDesign,WebDev,Design&via=WebDevSuren&url=http://gradients.online&text=Quickly%20generate%20beautiful%20gradients%20you%20can%20share%20with%20your%20friends!&">tweets!</a></p></div>');
 }
 
 //Closes alert.
